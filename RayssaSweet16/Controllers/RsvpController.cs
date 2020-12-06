e25092c65f8c09b6652fb06bb3771f88821b23d8
Jairo C. Assis <jairochvs@hotmail.com>
2020-12-06 14:39:05 -0500

    Converted PHP/HTML project to a .Net Core project.

---
+++ RayssaSweet16/Controllers/RsvpController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RayssaSweet16.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class RsvpController : ControllerBase
+    {
+        [HttpGet]
+        public string GetTestMessage()
+        {
+            return "Done";
+        }
+
+        [HttpPost]
+        public string SetRsvp([FromForm] SetRsvpRequest request)
+        {
+            Console.WriteLine(request);
+
+            return $"All went well [{request.Name}, {request.Email}, {request.NumberOfGuests}]";
+        }
+    }
+
+    public class SetRsvpRequest
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int NumberOfGuests { get; set; }
+    }
+}
--- php/config.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-// CONTACT FORM RECIPIENT MAIL SETUP
-define( 'CONTACT_FORM_RECIPIENT', 'example@youremail.com');
-
-?>
